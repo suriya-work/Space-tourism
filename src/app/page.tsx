ed7d917ed368079b49cdf85a8095d2a41c8c3d0c
mrprg <mohamadrezaasadi322@gmail.com>
2024-06-17 10:22:58 +0330

    add MotionDiv component to handle basic FADE animation as default

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
+import desktop from '@/assets/home/background-home-desktop.jpg'
+import mobile from '@/assets/home/background-home-mobile.jpg'
+import tablet from '@/assets/home/background-home-tablet.jpg'
+import Background from '@/components/Background'
+import MotionDiv from '@/components/MotionDiv'
+
 export default function Home() {
   return (
-    <div>
-      <p className=' text-white text-xl text-center'>
-        this is a text for test styles{' '}
-      </p>
-    </div>
+    <MotionDiv>
+      <Background
+        mobile={mobile.src}
+        tablet={tablet.src}
+        desktop={desktop.src}
+      />
+      <div className='relative z-30'>
+        <p className=' text-white text-xl text-center'>
+          this is a text for test styles{' '}
+        </p>
+      </div>
+    </MotionDiv>
   )
 }
