433a7ed990f12f2add79b5eecb741b67135f7701
suriya babaei <suriyalifework60@gmail.com>
2024-06-19 12:59:27 +0330

    fixed navbar create size tablet

--- src/components/globals/navbar/index.tsx
+++ src/components/globals/navbar/index.tsx
@@ -21,12 +21,12 @@ export default function Navbar() {
   const pathname = usePathname()
 
   return (
-    <header className='relative md:mt-16 z-10'>
-      <div className='absolute ml-12 mt-5'>
+    <header className='relative lg:mt-16 z-10 '>
+      <div className='absolute ml-12 mt-5 '>
         <Image src={logo} width={48} height={48} alt='Logo' />
       </div>
-      <div className='absolute left-48 right-0  w-[500px] md:flex hidden top-[40px] border-t border-white-5'></div>
-      <ul className='bg-white-5 flex justify-center gap-20 items-center md:w-[736px] md:h-[76px] absolute right-0'>
+      <div className='absolute left-48 right-0  w-[500px] lg:flex hidden top-[40px] border-t border-white-5'></div>
+      <ul className='bg-white-5 flex justify-center gap-20 items-center lg:w-[736px] w-[640px] h-[76px] absolute right-0'>
         {ListItemsNavbar.map((item, index) => {
           const isActive = pathname === item.href
           return (
