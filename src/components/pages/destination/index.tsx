defcc2967afa5972a2e6e0f22a9459e03b8e9324
mrprg <mohamadrezaasadi322@gmail.com>
2024-06-20 20:54:52 +0330

    add page title component and add it to destination and crew page

--- src/components/pages/destination/index.tsx
+++ src/components/pages/destination/index.tsx
@@ -1,6 +1,10 @@
+import PageTitle from '@/components/globals/PageTitle'
+
 const DestinationPage = () => {
   return (
     <div>
+      <PageTitle number={2} title='Meet Your crew' />
+
       <p className=''></p>
     </div>
   )
