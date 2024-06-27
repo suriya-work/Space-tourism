559b5431c68ae22d507611f75928578092a5b3cd
mrprg <mohamadrezaasadi322@gmail.com>
2024-06-27 20:03:17 +0330

    add new styles to sign in page

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -4,11 +4,11 @@ import SpaceShipAnim from '@/components/animations/SpaceShipAnim'
 
 const SignInPage = () => {
   return (
-    <div className='flex flex-col items-center lg:justify-center w-full lg:h-screen'>
+    <div className='flex flex-col items-center lg:justify-center w-full lg:h-screen '>
       <SpaceShipAnim />
-      <p>this is sign in page</p>
-
-      <SignIn />
+      <div className='pt-12 lg:pt-5'>
+        <SignIn />
+      </div>
     </div>
   )
 }
