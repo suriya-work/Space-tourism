64a2f4236bb993626b597608e15eec8038310c30
mrprg <mohamadrezaasadi322@gmail.com>
2024-06-27 11:21:39 +0330

    add publishable key to project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,9 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
       <html lang='en'>
         <Head>
           <meta name='application-name' content='SpaceShip Tourism' />
