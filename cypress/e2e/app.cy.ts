9de73bb3d0cc1afa4070e65345b367c06ee1535c
mrprg <mohamadrezaasadi322@gmail.com>
2024-06-29 10:52:33 +0330

    add test for sign in and protected destination page route

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -1,12 +1,17 @@
 describe('Navigation', () => {
-  it('should navigate to the about page', () => {
+  it('should navigate to the destination page after logging in', () => {
     // Start from the index page
     cy.visit('http://localhost:3000/')
+    // Find a link with an href attribute containing "destination" and click it
 
-    // Find a link with an href attribute containing "about" and click it
     cy.get('a[href*="/destination"]').click()
 
-    // The new url should include "/about"
+    cy.get('input[name=identifier]').type('mmdrezaasadi1998@gmail.com')
+    cy.get('button[data-localization-key="formButtonPrimary"]').click()
+    cy.get('input[name=password]').type('asadi23577')
+    cy.get('button[data-localization-key="formButtonPrimary"]').click()
+
+    // The new url should include "/destination"
     cy.url().should('include', '/destination')
   })
 })
